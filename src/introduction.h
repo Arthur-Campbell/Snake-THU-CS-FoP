4a1139965ed7afccbcd36328e41324b1590455f0
Arthur-Campbell <126866567+Arthur-Campbell@users.noreply.github.com>
2023-12-23 23:24:52 +0800

    Delete src directory

--- src/introduction.h
+++
@@ -1,4 +0,0 @@
-#ifndef INTRODUCTION_H
-#define INTRODUCTION_H
-void introduce();
-#endif
--- src/print.h
+++
@@ -1,8 +0,0 @@
-/// @brief 这部分是打印部分，用于在控制台光标操控下打印游戏界面
-#ifndef PRINT_H
-#define PRINT_H
-
-void gotoxy(short x, short y);
-
-void HideCursor();
-#endif  // PRINT_H
